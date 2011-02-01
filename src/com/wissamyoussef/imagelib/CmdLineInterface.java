c341a07c44742c23060fb751a733ac2b4a7b429a
Wissam Youssef <kodder@gmail.com>
2011-02-01 01:08:58 -0500

    first commit

---
+++ src/com/wissamyoussef/imagelib/CmdLineInterface.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.wissamyoussef.imagelib;
+
+/**
+ * @author kod
+ *
+ */
+public class CmdLineInterface {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
