887f6a0a4d957e603b5b6bff44cf854b875d8f82
Wissam Youssef <kodder@gmail.com>
2011-02-01 01:54:47 -0500

    Command line test

--- src/com/wissamyoussef/imagelib/CmdLineInterface.java
+++ src/com/wissamyoussef/imagelib/CmdLineInterface.java
@@ -1,20 +1,37 @@
 /**
- * 
+ * my package yay :P
  */
 package com.wissamyoussef.imagelib;
 
 /**
- * @author kod
+ * Imports my dear...obvious eh?
+ */
+
+import org.apache.commons.cli.*;
+
+/**
+ * @author Wissam Youssef
  *
  */
 public class CmdLineInterface {
 
 	/**
 	 * @param args
+	 * @throws ParseException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ParseException {
+		// This is gonna be fun :)
+		Options options = new Options();
+		
+		options.addOption("t", false, "display current time");
+		CommandLineParser parser = new PosixParser();
+		CommandLine cmd = parser.parse( options, args);
+		if(cmd.hasOption("t")) {
+		    System.out.println("bleh");
+		}
+		else {
+		    System.out.println("fuckkkk");
+		}
 	}
 
 }
