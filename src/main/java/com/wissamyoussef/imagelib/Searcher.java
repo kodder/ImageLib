b17ec3c1308221904a9855002608187df0829d15
Wissam Youssef <wissam@mate1inc.com>
2011-02-21 14:44:50 -0500

    Added searcher class,  it searches for specific images

---
+++ src/main/java/com/wissamyoussef/imagelib/Searcher.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.wissamyoussef.imagelib;
+
+/**
+ * @author Wissam Youssef
+ *
+ */
+public class Searcher
+{
+
+	/**
+	 * 
+	 */
+	public Searcher()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
